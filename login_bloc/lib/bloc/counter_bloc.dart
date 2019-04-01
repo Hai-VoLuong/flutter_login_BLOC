6987f945345541b95c9a47f939416c2e5382eeeb
Võ Hải <hai.softech@gmail.com>
2019-04-01 15:43:35 +0700

    change value use StreamTransformer

--- login_bloc/lib/bloc/counter_bloc.dart
+++ login_bloc/lib/bloc/counter_bloc.dart
@@ -4,7 +4,13 @@ class CounterBloc {
   int _counter = 0;
 
   StreamController _counterStreamController = new StreamController<int>();
-  Stream get counterStream => _counterStreamController.stream;
+  Stream get counterStream => _counterStreamController.stream.transform(_counterTransformer);
+
+  // change value
+  var _counterTransformer = StreamTransformer<int, int>.fromHandlers(handleData: (data, sink) {
+    data += 10;
+    sink.add(data);
+  });
 
   void increment() {
     _counter += 1;
